bac26afd6c02c7eebc6ebbb294961834dbdeb2d4
Pak <radishlau@gmail.com>
2016-11-18 13:27:34 +0800

    success

--- yue-cuhk/ViewController.swift
+++ yue-cuhk/ViewController.swift
@@ -26,43 +26,74 @@ class ViewController: UIViewController {
     }
     
     func getHtml(){
-//        let url = "http://pakwlau.com/yue/cu.html"
-        let url = "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/guide.php"
+        let big5CfIndex = CFIndex(CFStringEncodings.big5.rawValue)
+        let big5 = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(big5CfIndex))
+        let keyword = "文"
+        let escapedKeyword = keyword.addingPercentEscapes(using: String.Encoding(rawValue: big5))
+
+        let urlStr = "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/search.php?q=" + escapedKeyword!
+        let url = URL(string:urlStr)
+        let data = NSData(contentsOf: url!)
         
-        //methodDependent
-        Alamofire.request(url).responseString(encoding: String.Encoding.utf8) { (response) in
-            print("Result: ",response.result.error)
-            // if sucess
-            guard (response.result.isSuccess) else {return}
-            let result = response.result.value!
-            
-           
-            
-            print(result)
-            //self.parseHtml(html: response.result.value!)
-        }
+        let html = NSString(data: data! as Data, encoding: big5) as! String
+        parseHtml(html: html)
     }
     
     func parseHtml(html:String){
         print("======= Begin Parsing ======= ")
- 
+        
         if let doc = Kanna.HTML(html:html , encoding: .utf8){
             
-            // Search for nodes by CSS
-//            for link in doc.css("div nowrap") {
-//                print(link.text)
-//            }
-            
-            let data = doc.css("tr td div")
-            let first = data.first!.innerHTML!
-            let firstLineBreakIndex = first.range(of: "\n")?.lowerBound
-            let content = first.substring(to: firstLineBreakIndex!)
-            
-            print("first ",content)
-            print("second ",data[1].content)
+            for tr in doc.css("form table tr"){
+                // remove first and last result
+                if (tr.css("td[align^='center']").first != nil ){
+                    for td in tr.css("td"){
+                        let content = td.content?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                        print(content)
+                    }
+                }
+                print("\n")
+            }
 
+           
+            
+            
+            
+            
+            
+            
+            
+//            let first = data.first!.innerHTML!
+//            if let firstLineBreakIndex = first.range(of: "<")?.lowerBound{
+//                 firstContent = first.substring(to: firstLineBreakIndex)
+//            }else{
+//                firstContent = data.first?.content
+//            }
+//            
+//            
+//            print("first ",firstContent)
+//            print("second ",data[1].content)
+            
         }
     }
     
 }
 
+
+extension Data {
+    var stringValue: String? {
+        return String(data: self, encoding: .utf8)
+    }
+    var base64EncodedString: String? {
+        return base64EncodedString(options: .lineLength64Characters)
+    }
+}
+extension String {
+    var utf8StringEncodedData: Data? {
+        return data(using: .utf8)
+    }
+    
+    var base64DecodedData: Data? {
+        return Data(base64Encoded: self, options: .ignoreUnknownCharacters)
+    }
+}
