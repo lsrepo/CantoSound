ebb0d68a3bb977edd0572b5961a6624f01b44496
Pak <radishlau@gmail.com>
2020-09-20 14:06:56 +0800

    Create v2

---
+++ v2/CantoSound/CantoSound/CantoSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CantoSoundApp.swift
+//  CantoSound
+//
+//  Created by Pak Lau on 20/9/2020.
+//
+
+import SwiftUI
+
+@main
+struct CantoSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ v2/CantoSound/CantoSound/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CantoSound
+//
+//  Created by Pak Lau on 20/9/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
