4c39240d18f4f981c27d010c42bdb5cd901ebc05
Pak <radishlau@gmail.com>
2020-09-21 20:57:07 +0800

    Improve UI

--- v2/CantoSound/CantoSound/ContentView.swift
+++ v2/CantoSound/CantoSound/ContentView.swift
@@ -7,12 +7,24 @@
 
 import SwiftUI
 
+struct MyTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(
+                RoundedRectangle(cornerRadius: 3, style: .continuous)
+                    .stroke(Color.white, lineWidth: 2)
+            ).padding()
+    }
+}
+
 struct ContentView: View {
     @State var capturedImage = UIImage()
     @State var detectedWords = [Word]()
-
+    @State var keyword = ""
+    @State var showCameraView = false
+    
     func handleText(sentences: [String]) {
-
         let characters = sentences.flatMap{ sentence in Array(sentence) }
         let words = characters.map{ character in Word(character: String(character))}
         detectedWords = words
@@ -27,31 +39,55 @@ struct ContentView: View {
     }
     
     var cameraView = CameraView()
-
+    
     var body: some View {
         VStack {
+            HStack{
+                TextField("打字", text: $keyword)
+                    .textFieldStyle(MyTextFieldStyle())
+                    .keyboardType(.default)
+                    .font(.largeTitle)
+                    .lineLimit(1)
+                    .frame(width: 150, height: 150.0)
+                    .padding()
+                Text("或")
+                    .padding()
+                Button(
+                    action : {
+                        showCameraView = true
+                    },
+                    label : { Image(systemName: "camera.viewfinder")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame( width:40, height: 40)}
+                )
+                .padding()
+            }
+            WordList(words: $detectedWords)
+        }.sheet(isPresented: $showCameraView, content: {
             cameraView
                 .frame(maxHeight: 200)
             Button(
                 action : {
                     cameraView.controller.photoCaptureCompletionBlock = handlePhotoReceived
                     cameraView.controller.capturePhoto()
+                    showCameraView = false
                 },
                 label : {Text("Detect")
-                })
-                .frame(height: 200)
-//
-//            Image(uiImage: capturedImage)
-//                .resizable().aspectRatio(contentMode: .fit)
-//                .frame(height:200)
-//                .foregroundColor(.black)
-            WordList(words: $detectedWords)
-        }
+            })
+            .frame(height: 200)
+            
+        })
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+        }
     }
 }
