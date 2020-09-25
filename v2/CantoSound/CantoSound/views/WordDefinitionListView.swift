acac9146b573fa99a3f76b3fc7d32cb17bcbc3c6
Pak <radishlau@gmail.com>
2020-09-25 23:29:39 +0800

    Show note in Definition Row

--- v2/CantoSound/CantoSound/views/WordDefinitionListView.swift
+++ v2/CantoSound/CantoSound/views/WordDefinitionListView.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 struct WordDefinitionListView: View {
     @Binding var definitions: [ChineseWordefinition]
-   
+    
     var body: some View {
-        List{
-            ForEach(definitions) { definition in
-                WordDefinitionRow(definition: definition)
+        List {
+            ForEach(definitions) {
+                definition in WordDefinitionRow(definition: WordDefinitionRowData(
+                    homophone: definition.homophones.first ?? "",
+                    syllabel: definition.syllableYale ?? "",
+                    longText: definition.words
+                        .joined(separator: ", ")
+                        .appending(definition.words.isEmpty ? "" : " ")
+                        .appending(definition.note ?? "")
+                )
+                )
             }
-         }
+        }
     }
+}
+
+struct WordDefinitionRowData {
+    var homophone: String
+    var syllabel: String
+    var longText: String
+    
 }
 
 struct WordDefinitionListView_Previews: PreviewProvider {
     static var example = [ChineseWordefinition(
         syllableYale: "oi3",
         homophones: ["鑀", "焥", "薆"],
-        words: ["愛心", "愛情", "愛護", "愛惜", "博愛", "熱愛", "偏愛", "疼愛"]
+        words: ["愛心", "愛情", "愛護", "愛惜", "博愛", "熱愛", "偏愛", "疼愛"],
+        note: "異讀字"
     )]
     @Binding var definitions: [ChineseWordefinition]
     
@@ -34,17 +50,18 @@ struct WordDefinitionListView_Previews: PreviewProvider {
 }
 
 struct WordDefinitionRow: View, Identifiable {
-    var definition: ChineseWordefinition
+    var definition: WordDefinitionRowData
     var id = UUID()
+
     var body: some View {
         HStack{
             VStack{
-                Text(definition.homophones.first ?? "")
+                Text(definition.homophone)
                     .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                Text(definition.syllableYale ?? "")
+                Text(definition.syllabel)
                     .font(.title3)
             }.padding()
-            Text(definition.words.joined(separator: ", "))
+            Text(definition.longText)
         }
     }
 }
